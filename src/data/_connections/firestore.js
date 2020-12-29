5439b21e29681c387afc8ec20d4216c1741318aa
Jeff Fasulkey <fazulk@gmail.com>
2020-12-28 22:58:06 -0800

    Updates

--- src/data/_connections/firestore.js
+++ src/data/_connections/firestore.js
@@ -1,5 +1,10 @@
 import Firestore from '@google-cloud/firestore'
-//
+
 export const fasulkeyDb = () => {
-    return new Firestore()
+    return process.env.NODE_ENV === `development`
+        ? new Firestore({
+              projectId: `fasulkey`,
+              keyFilename: `../fasulkey-fire-store.json`
+          })
+        : new Firestore()
 }
